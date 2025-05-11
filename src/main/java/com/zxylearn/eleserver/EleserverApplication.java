030bf2e839f1b478b7399f458d2f978c84c7663e
failedman <2711339704@qq.com>
2025-05-11 18:56:00 +0800

    ctojava

---
+++ src/main/java/com/zxylearn/eleserver/EleserverApplication.java
@@ -0,0 +1,13 @@
+package com.zxylearn.eleserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EleserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EleserverApplication.class, args);
+    }
+
+}
